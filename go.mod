df4e29138ae3b97d030ec3d1148ee9dd4a66cb0d
Gucheng Wang <nomeguy@qq.com>
2021-10-30 23:31:31 +0800

    Can get bat processes.

--- go.mod
+++ go.mod
@@ -7,7 +7,9 @@ require (
 	github.com/casdoor/casdoor-go-sdk v0.0.12-FIX
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/google/go-github/v38 v38.0.0
+	github.com/melbahja/goph v1.2.1
 	github.com/mileusna/crontab v1.0.1
+	golang.org/x/crypto v0.0.0-20201217014255-9d1352758620
 	golang.org/x/oauth2 v0.0.0-20210514164344-f6687ab2804c
 	xorm.io/core v0.7.2
 	xorm.io/xorm v0.8.1
---
+++ ssh/ssh.go
@@ -0,0 +1,47 @@
+// Copyright 2021 The casbin Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ssh
+
+import (
+	"net"
+
+	"github.com/melbahja/goph"
+	"golang.org/x/crypto/ssh"
+)
+
+func RunCommand(ip string, username string, password string, command string) string {
+	client, err := goph.NewConn(&goph.Config{
+		User:    username,
+		Addr:    ip,
+		Port:    22,
+		Auth:    goph.Password(password),
+		Timeout: goph.DefaultTimeout,
+		Callback: func(host string, remote net.Addr, key ssh.PublicKey) error {
+			return nil
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+	defer client.Close()
+
+	out, err := client.Run(command)
+	if err != nil {
+		panic(err)
+	}
+
+	res := string(out)
+	return res
+}
